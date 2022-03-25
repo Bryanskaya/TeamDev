094d751380cf4954e36de4cb016358bd198da8b6
Bryanskaya <bryanskayakatya@yandex.ru>
2022-03-25 23:54:36 +0300

    ADD: main container for cite content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import Fonts from "./components/Fonts";
+import Home from "./components/layout/Home";
 import theme from "./extendTheme";
 import { ChakraProvider, Box, Text, Menu, Container } from "@chakra-ui/react";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Fonts />
-    Hello world
+    Hello worl
+    <Home />
   </ChakraProvider>
 );
