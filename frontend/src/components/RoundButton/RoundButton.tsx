543f2bf581895acf6c5907ee7d817d5757715065
fairay <48217178+fairay@users.noreply.github.com>
2022-04-08 01:02:37 +0300

    Extend: text and shape styles

--- frontend/src/components/RoundButton/RoundButton.tsx
+++ frontend/src/components/RoundButton/RoundButton.tsx
@@ -1,32 +1,19 @@
 import React from "react";
 
 import {
-    Button, ButtonProps, chakra, ChakraProvider, extendTheme
+    ButtonProps, chakra
 } from "@chakra-ui/react";
 
 import theme from "styles/extendTheme";
 import styles from "./RoundButton.module.scss";
 
-interface RoundButtonProps extends ButtonProps {
-    focusBorderColor?: string
-    focusColor?: string
-    focusBgColor?: string
-}
+interface RoundButtonProps extends ButtonProps {}
 
 const RoundButton: React.FC<RoundButtonProps> = (props) => {
-    const { 
-        border="1px solid",  borderRadius="10px",
-        display="flex",
-        focusBorderColor = theme.colors["accent-3"],
-        focusColor = theme.colors["text"],
-        focusBgColor = theme.colors["bg"],
-        ...rest 
-    } = props
-
     return (
         <chakra.button 
             className={styles.round_button} 
-            {...rest}
+            {...props}
         />
     )
 }
