5027f6b26648614a3abe6539e53a84e10ea87e8d
fairay <48217178+fairay@users.noreply.github.com>
2022-04-10 20:17:02 +0300

    Add: icons components

---
+++ frontend/src/components/Icons/Logo.tsx
@@ -0,0 +1,11 @@
+import { SVGProps } from "react";
+
+interface LogoProps extends SVGProps<SVGElement> {
+  width?: string,
+  height?: string,
+  fill?: string,
+  visibility?: string
+}
+
+
+export default LogoProps
