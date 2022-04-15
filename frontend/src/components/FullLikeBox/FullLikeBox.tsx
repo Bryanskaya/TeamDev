18f47564f77f398213e843adce553a57b01d68cd
Bryanskaya <bryanskayakatya@yandex.ru>
2022-04-16 00:21:16 +0300

    Add: new components - FullLike

---
+++ frontend/src/components/FullLikeBox/FullLikeBox.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+    InputProps as IProps, Input as I,
+    Box, Text
+} from "@chakra-ui/react";
+import FullLike from "components/Icons/FullLike";
+
+import styles from "./FullLikeBox.module.scss";
+
+interface InputProps extends IProps {
+    likesNum?: number
+}
+
+const FullLikeBox: React.FC<InputProps> = (props) => {
+    return (
+    <Box className={styles.likes_box}> 
+        <Box> <FullLike /> </Box>
+        <Text> {props.likesNum} </Text>
+    </Box>
+    )
+}
+
+export default FullLikeBox;
