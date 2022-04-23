69fce40223b5b839d8e573da1e3baf959c5ffc5e
Bryanskaya <bryanskayakatya@yandex.ru>
2022-04-24 01:03:53 +0300

    Refactor: user's card

--- frontend/src/components/UserCard/UserCard.tsx
+++ frontend/src/components/UserCard/UserCard.tsx
@@ -1,59 +1,51 @@
 import { Box, HStack, Image, Link, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
-import {Account as AccountI} from "types/Account";
+import { Account as AccountI } from "types/Account";
 
 import FullLikeBox from "components/Boxes/FullLike/FullLikeBox";
+import RoleBox from "components/Boxes/Role/RoleBox";
 import LoginBox from "components/LoginBox";
 import RecipeBox from "components/RecipeBox";
 import user from "img/user.png";
 
 import styles from "./UserCard.module.scss";
 
-interface UserProps extends AccountI {}
-
+interface UserProps extends AccountI {
+    role: string
+}
 
 const UserCard: React.FC<UserProps> = (props) => {
-    const [likes, change] = React.useState(0);
-    const pathRecipes = "/accounts/" + props.login + "/recipes";
-    const pathLiked = "/accounts/" + props.login + "/like";
+  const pathRecipes = "/accounts/" + props.login + "/recipes";
+  const pathLiked = "/accounts/" + props.login + "/likes";
 
-    async function getLikes() {
-        // var data = await GetLikes(props.id)
-        // if (data.status === 200) {
-        //     change(data.content)
-        // }
-    }
+  return (
+    <Box className={styles.main_box}>
+      <HStack>
+        <VStack>
+          <Image src={user} />
 
-    getLikes();
-    
-    return (
-        <Box className={styles.main_box}>
-            <HStack>
-                <VStack>
-                    <Image src={user}/>
-                    <VStack className={styles.role}>
-                        <Text>Роль</Text>
-                        <FullLikeBox likesNum={"Понравилось"}/>
-                    </VStack>
-                </VStack>
+          <VStack className={styles.role}>
+            <Text>Роль</Text>
+            <RoleBox login={props.login} role={props.role}/>
+          </VStack>
+        </VStack>
 
-                <VStack className={styles.info}>
-                    <Text>Логин</Text>
-                    <LoginBox login={props.login} className={styles.login}/>
+        <VStack className={styles.info}>
+          <Text>Логин</Text>
+          <LoginBox login={props.login} className={styles.login} />
 
-                    <Link href={pathRecipes}>
-                        <RecipeBox data={"Рецепты"} className={styles.recipes}/>
-                    </Link>
+          <Link href={pathRecipes}>
+            <RecipeBox data={"Рецепты"} className={styles.recipes} />
+          </Link>
 
-                    <Link href={pathLiked}> 
-                        <FullLikeBox likesNum={"Понравилось"}/>
-                    </Link>
-                </VStack> 
-
-            </HStack>
-        </Box>
-    )
-}
+          <Link href={pathLiked}>
+            <FullLikeBox likesNum={"Понравилось"} />
+          </Link>
+        </VStack>
+      </HStack>
+    </Box>
+  );
+};
 
 export default UserCard;
