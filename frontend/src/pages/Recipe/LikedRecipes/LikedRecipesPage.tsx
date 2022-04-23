69aa42f38970fa17f367637569d1fbecb91c34d6
Bryanskaya <bryanskayakatya@yandex.ru>
2022-04-23 22:33:18 +0300

    Add: posibility to use page not only for login in cookie

--- frontend/src/pages/Recipe/LikedRecipes/LikedRecipesPage.tsx
+++ frontend/src/pages/Recipe/LikedRecipes/LikedRecipesPage.tsx
@@ -2,6 +2,7 @@ import { Box } from "@chakra-ui/react";
 import GetRecipes from "postAPI/likes/GetRecipes";
 import React from "react";
 import { useCookies } from "react-cookie";
+import { useParams } from "react-router-dom";
 import RecipeMap from "../../../components/RecipeMap/RecipeMap";
 
 import styles from "./LikedRecipesPage.module.scss";
@@ -9,13 +10,15 @@ import styles from "./LikedRecipesPage.module.scss";
 interface LikedRecipesProps {}
 
 const LikedRecipes: React.FC<LikedRecipesProps> = (props) => {
-    let [cookie] = useCookies(['role', 'login']);
+  let [cookie] = useCookies(["role", "login"]);
+  const params = useParams();
+  let login = params.login ? params.login : cookie.login;
 
-    return (
+  return (
     <Box className={styles.main_box}>
-        <RecipeMap getCall={() => GetRecipes(cookie.login)}/>
+      <RecipeMap getCall={() => GetRecipes(login)} />
     </Box>
-    );
+  );
 };
 
 export default LikedRecipes;
